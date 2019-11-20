cd347b9d0eba02dcd8a1bfecc270dfd1b187154e
Richard Howlett <richardhowlett14@gmail.com>
2019-11-20 10:57:59 -0500

    Exercise 4 Complete and Added Styling

--- world.php
+++ world.php
@@ -16,9 +16,29 @@ if ($country == ""){
 	$stmt->execute();
 	$results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 }
-?>
-<ul>
+/*?>
+ <ul>
 <?php foreach ($results as $row): ?>
   <li><?= $row['name'] . ' is ruled by ' . $row['head_of_state']; ?></li>
 <?php endforeach; ?>
-</ul>
+</ul> 
+*/
+?>
+<table class="table">
+		<thead class="head">
+			<tr >
+				<th >Name</th>
+				<th >Continent</th>
+				<th >Independence</th>
+				<th >Head of State</th>
+			</tr>
+		</thead>
+		<?php foreach ($results as $row): ?>
+			<tr class="row">
+				<td  ><?= $row['name']; ?></td>
+				<td  ><?= $row['continent']; ?></td>
+				<td  ><?= $row['independence_year']; ?></td>
+				<td  ><?= $row['head_of_state'];?></td>
+			</tr>
+		<?php endforeach; ?>
+</table>
