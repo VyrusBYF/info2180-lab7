c3700f62cdd106407906897127b6ae36cd4206ea
Richard Howlett <richardhowlett14@gmail.com>
2019-11-20 15:05:14 -0500

    Exercise 5 Complete

--- world.js
+++ world.js
@@ -1,5 +1,7 @@
 window.onload = function (){
-	var lookup = document.getElementById("lookup").addEventListener("click",birthPlace);
+	
+	var findCountry = document.getElementById("lookup").addEventListener("click",birthPlace);
+	var findCity = document.getElementById("city").addEventListener("click",City);
 
 	function birthPlace() {
 
@@ -25,4 +27,31 @@ window.onload = function (){
 		httpRequest.open('GET', url+"?country="+country);
 		httpRequest.send();		
 	}
+
+	function City() {
+
+		var input = document.getElementById("country").value;
+
+		//Value is formatted to ensure the request is completed as long as the country is correctly spelled
+		var country = input.charAt(0).toUpperCase() + input.toLowerCase().slice(1);
+		var city = "&context=cities";
+		//console.log(country);
+		var httpRequest = new XMLHttpRequest();
+		var url = "world.php";
+
+		httpRequest.onreadystatechange = function (){
+			if (httpRequest.readyState === XMLHttpRequest.DONE) {
+			 	if (httpRequest.status === 200) {
+			 		var response = httpRequest.responseText;
+		 			document.getElementById("result").innerHTML=response;
+
+			 	} else {
+			 		alert('There was a problem with the request.');
+			 	}
+			}
+		};
+		httpRequest.open('GET', url+"?country="+country+city);
+		httpRequest.send();		
+	}
+
 }
--- world.php
+++ world.php
@@ -1,4 +1,4 @@
-<?php
+<?php error_reporting(E_ERROR | E_WARNING | E_PARSE); 
 $host = getenv('IP');
 $username = 'root';
 $password = 'naruto';
@@ -7,15 +7,33 @@ $dbname = 'world';
 $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
 #$stmt = $conn->query("SELECT * FROM countries");
 $country = ucfirst(strtolower($_REQUEST["country"]));
+$context = $_REQUEST["context"];
+
 #print $country;
+#print $context;
 if ($country == ""){
 	$stmt = $conn->query("SELECT * FROM countries");
 	$results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+}elseif($context !=""){
+	$stmt = $conn->prepare("SELECT code FROM countries WHERE name LIKE '%$country%'");
+	$stmt->execute();
+	$results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+	#print_r ($results);
+	
+	$code = $results[0]['code'];
+	$newstmt =$conn->prepare("SELECT * FROM cities WHERE country_code LIKE '%$code%'");
+	$newstmt->execute();
+	
+	$results = $newstmt->fetchAll(PDO::FETCH_ASSOC);
+	#print_r ($results);
+
 }else{
 	$stmt = $conn->prepare("SELECT * FROM countries WHERE name LIKE '%$country%'");
 	$stmt->execute();
 	$results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+	#print_r ($results);
 }
+
 /*?>
  <ul>
 <?php foreach ($results as $row): ?>
@@ -23,8 +41,11 @@ if ($country == ""){
 <?php endforeach; ?>
 </ul> 
 */
+#print $context; ?>
+
+<?php if(empty($context)){
 ?>
-<table class="table">
+    <table class="table">
 		<thead class="head">
 			<tr >
 				<th >Name</th>
@@ -41,4 +62,23 @@ if ($country == ""){
 				<td  ><?= $row['head_of_state'];?></td>
 			</tr>
 		<?php endforeach; ?>
-</table>
+	</table>
+<?php }else{ ?>
+    <table class="table">
+		<thead class="head">
+			<tr >
+				<th >Name</th>
+				<th >District</th>
+				<th >Population</th>
+			</tr>
+		</thead>
+		<?php foreach ($results as $row): ?>
+			<tr class="row">
+				<td  ><?= $row['name']; ?></td>
+				<td  ><?= $row['district']; ?></td>
+				<td  ><?= $row['population']; ?></td>
+			</tr>
+		<?php endforeach; ?>
+	</table>
+<?php } ?>
+
